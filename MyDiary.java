16baf34b19534226e6809342e9ae92863105b2ad
Yuna0203 <citron5553@gmail.com>
2022-05-25 14:03:51 +0000

    MyDiary

---
+++ MyDiary.java
@@ -0,0 +1,9 @@
+public class MyDiary {
+  public static void main(string[] args){
+    int version = 1 + 5;
+    System.out.println("日記ソフト");
+    System.out.println("ver" + version);
+    System.out.println("まだ開発中です");
+    System.out.println("終了します");
+  }
+}
